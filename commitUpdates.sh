030ea9e515b2eb23e63d2340ab9c3db25886d196
akpetty <alekpetty@gmail.com>
2022-03-29 12:16:00 -0400

    update

---
+++ commitUpdates.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source activate urubu
+make 
+
+echo -e "build"
+cp -R _build/* ../earth2you.github.io
+
+echo -e "commit to Git"
+cd ../earth2you.github.io
+
+echo -e "pull any missing commits"
+git pull
+
+git add --all
+git commit -m "update"
+echo -e "Pushing to earth2you.github.io"
+git push origin master
+
+echo -e "Done"
+
+
